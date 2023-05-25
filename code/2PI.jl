622568928fe5a212daea38dd36405e5f1e24b259
Carson C Chow <ccc1685@users.noreply.github.com>
2023-05-25 10:07:18 -0400

    updates

--- code/2PI.jl
+++ code/2PI.jl
@@ -7,6 +7,19 @@ Fu(phi) = 1+cos(phi)
 dF(a,a1,I,phi,phi1,dphi) = (F(phi,I)*a - F(phi1,I)*a1)/dphi
 dFu(a,a1,phi,phi1,dphi) = (Fu(phi)*a - Fu(phi1)*a1)/dphi
 
+transform(phi,I) =2*atan(sqrt(I)*tan(phi/2))
+transform_inv(v,I) =2*atan(tan(v/2)/sqrt(I))
+
+function transform_a3(a3,phi,I)
+    N = length(a3)
+    at = similar(a3)
+    for (i,a) in enumerate(a3)
+        ia = phase_index(transform_inv(phi[i],I),N)
+        at[ia] = a
+    end
+end
+
+
 
 """
     step_a3!(a3,C31,I,sigma2,h,N,phi,dphi)
@@ -35,6 +48,195 @@ function step_a3!(a3,C31,I,sigma2,h,N,dphi,T)
     end
 end
 
+function step_1s(a3,C11,D11,C31,D31,C33,D33,t,sigma2,nu,h,N,dphi,indpi)
+    D11 += h*(nu^2*beta2*sigma2*C33[indpi,indpi] - beta*D11)
+    C11 += h*(D11 - beta*C11)
+    for indchi in 1:N
+        phi = mod2pi(indchi*dphi + nu*t)
+        indphi = floor(Int,phi/dphi)
+        indphi = indphi == 0 ? N : indphi
+        indphi1 = indphi == 1 ? N : indphi - 1
+        dGa3 = (G(phi,nu)*a3[indphi] - G(phi-dphi,nu)*a3[indphi1])/dphi
+        D31[indchi] -= h*dGa3*D11
+        C31[indchi] += h*(D31[indchi] - beta*C31[indchi])
+        for indchip in 1:N
+            chip = mod2pi(indchip*dphi)
+            indchip1 = indchip == 1 ? N : indchip - 1
+            D33[indchi,indchip] += h*dGa3*(G(chip,nu)*a3[indchip] - G(chip-dphi,nu)*a3[indchip1])/dphi*C11
+        end
+    end
+    return C11,D11,C31,D31,.5*(D33+D33')
+end
+function step_2s(a3,C11,D11,C31,D31,C33,D33,t,sigma2,nu,h,N,dphi,indpi)
+
+    for indchi in 1:N
+        phi = mod2pi(indchi*dphi + nu*t)
+        indphi = floor(Int,phi/dphi)
+        indphi = indphi == 0 ? N : indphi
+        indphi1 = indphi == 1 ? N : indphi - 1
+        a3[indchi] -= h*sigma2*(G(phi,nu)*C31[indphi] - G(phi-dphi,nu)*C31[indphi1])/dphi
+        for indchip in 1:N
+            phip = mod2pi(indchip*dphi + nu*t)
+            indphip = floor(Int,phip/dphi)
+            indphip = indphip == 0 ? N : indphip
+            C33[indchi,indchip] += h*D33[indchi,indphip]
+        end
+    end
+    return a3,.5*(C33+C33')
+end
+
+
+function step_1(a3,C11,D11,C31,D31,C33,D33,t,sigma2,nu,h,N,dphi,indpi)
+    D11 += h*(nu^2*beta2*sigma2*C33[indpi,indpi] - beta*D11)
+    C11 += h*(D11 - beta*C11)
+    for indchi in 1:N
+        phi = mod2pi(indchi*dphi + nu*t)
+        indphi = floor(Int,phi/dphi)
+        indphi = indphi == 0 ? N : indphi
+        indphi1 = indphi + 1 > N ? 1 : indphi + 1
+        dGa3 = (G(phi+dphi,nu)*a3[indphi1] - G(phi,nu)*a3[indphi])/dphi
+        D31[indchi] -= h*dGa3*D11
+        C31[indchi] += h*(D31[indchi] - beta*C31[indchi])
+        for indchip in 1:N-1
+            chip = mod2pi(indchip*dphi)
+            D33[indchi,indchip] += h*dGa3*(G(chip+dphi,nu)*a3[indchip+1] - G(chip,nu)*a3[indchip])/dphi*C11
+        end
+        D33[indchi,N] += h*dGa3*(G(dphi,nu)*a3[1] - G(0,nu)*a3[N])/dphi*C11
+    end
+    return C11,D11,C31,D31,.5*(D33+D33')
+end
+function step_2(a3,C11,D11,C31,D31,C33,D33,t,sigma2,nu,h,N,dphi,indpi)
+
+    for indchi in 1:N
+        phi = mod2pi(indchi*dphi + nu*t)
+        indphi = floor(Int,phi/dphi)
+        indphi = indphi == 0 ? N : indphi
+        indphi1 = indphi + 1 > N ? 1 : indphi + 1
+        a3[indchi] -= h*sigma2*(G(phi+dphi,nu)*C31[indphi1] - G(phi,nu)*C31[indphi])/dphi
+        for indchip in 1:N
+            phip = mod2pi(indchip*dphi + nu*t)
+            indphip = floor(Int,phip/dphi)
+            indphip = indphip == 0 ? N : indphip
+            C33[indchi,indchip] += h*D33[indchi,indphip]
+        end
+    end
+    return a3,.5*(C33+C33')
+end
+
+function evolve(T,sigma2,nu,h,N,f)
+    a3 = 2pi/N .+ .2/N .* cos.(2pi*collect(1:N)/N)
+    # a3 = ones(N)*2pi/N
+    C31 = zeros(N)
+    D31 = zeros(N)
+    C33 = zeros(N,N)
+    # for i in 1:N
+    #     C33[i,i] = 1/N/N
+    # end
+    D33 = zeros(N,N)
+    C11 = 0.01
+    D11 = 0.
+    f(a3,C11,D11,C31,D31,C33,D33,T,sigma2,nu,h,N)
+end
+
+function rotateback(v::Vector,t,nu)
+    n = floor(Int,length(v)*t*nu/2pi)
+    circshift(v,n)
+end
+
+function rotateback(m::Matrix,t,nu)
+    n = floor(Int,size(m)[1]*t*nu/2pi)
+    circshift(m,(n,n))
+end
+
+
+function evolve_C33(a3,C11,D11,sigma2,nu,h,N)
+    a3t = a3
+    C31t = C31
+    dphi = 2pi /N
+    tau = 0
+    n = 1
+    x = [0.]
+    for t in 0:h:T
+        indpi = floor(Int,mod2pi(pi+nu*t)/dphi)
+        indpi = indpi == 0 ? N : indpi
+        a3,C11,D11,C31,D31,C33,D33 = step(a3,C11,D11,C31,D31,C33,D33,t,sigma2,nu,h,N,dphi,indpi)
+        if tau >= dphi/nu
+            a3t = hcat(a3t,circshift(a3,n))
+            C31t = hcat(C31t,circshift(C31,n))
+            push!(x,t)
+            tau -= dphi/nu
+            n += 1
+        end
+        tau += h
+    end
+    return rotateback(a3,T,nu),C11,D11,rotateback(C31,T,nu),rotateback(D31,T,nu),rotateback(C33,T,nu),rotateback(D33,T,nu),a3t,C31t,x,collect(1:N)
+end
+
+function evolves(a3,C11,D11,C31,D31,C33,D33,T,sigma2,nu,h,N)
+    a3t = a3
+    C31t = C31
+    dphi = 2pi /N
+    tau = 0
+    n = 1
+    x = [0.]
+    for t in 0:h:T
+        indpi = floor(Int,mod2pi(pi+nu*t)/dphi)
+        indpi = indpi == 0 ? N : indpi
+        a3,C11,D11,C31,D31,C33,D33 = steps(a3,C11,D11,C31,D31,C33,D33,t,sigma2,nu,h,N,dphi,indpi)
+        if tau >= dphi/nu
+            a3t = hcat(a3t,circshift(a3,n))
+            C31t = hcat(C31t,circshift(C31,n))
+            push!(x,t)
+            tau -= dphi/nu
+            n += 1
+        end
+        tau += h
+    end
+    return rotateback(a3,T,nu),C11,D11,rotateback(C31,T,nu),rotateback(D31,T,nu),rotateback(C33,T,nu),rotateback(D33,T,nu),a3t,C31t,x,collect(1:N)
+end
+
+
+function evolve_fd(a3,C11,D11,C31,D31,C33,D33,T,sigma2,nu,h,N)
+    a3t = a3
+    C31t = C31
+    dphi = 2pi / N
+    indpi = div(N,2)
+    tau = 0
+    x = [0.]
+    for t in 0:h:T
+        a3,C11,D11,C31,D31,C33,D33 = step_fd(a3,C11,D11,C31,D31,C33,D33,sigma2,nu,h,N,dphi,indpi)
+        if tau >= min(dphi/nu,1.)
+            a3t = hcat(a3t,a3)
+            C31t = hcat(C31t,C31)
+            push!(x,t)
+            tau -= dphi/nu
+        end
+        tau += h
+    end
+    return a3,C11,D11,C31,D31,C33,D33,a3t,C31t,x,collect(1:N)
+end
+
+
+function evolve_t(a3,C11,D11,C33in,D33in,T,sigma2,nu,h,N)
+    C33 = copy(C33in)
+    D33 = copy(D33in)
+    C11t = [C11]
+    dphi = 2pi /N
+    tau = 0
+    x = [0.]
+    for t in 0:h:T
+        indpi = floor(Int,mod2pi(pi+nu*t)/dphi)
+        indpi = indpi == 0 ? N : indpi
+        C11,D11,C33,D33 = step_t(a3,C11,D11,C33,D33,t,sigma2,nu,h,N,dphi,indpi)
+        if tau >= dphi/nu
+            push!(C11t,C11)
+            push!(x,t)
+            tau -= dphi/nu
+        end
+        tau += h
+    end
+    return C11t,x
+end
 
 
 
